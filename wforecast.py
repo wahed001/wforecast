1ffb53b45412734cf72478b0be36bfd6926db4db
Mohammed Abdullah <wahed001@hotmail.com>
2023-04-27 03:32:01 -0700

    Initial commit

---
+++ wforecast.py
@@ -0,0 +1,12 @@
+import requests
+
+class Weather:
+
+    def __init__(self):
+        r = requests.get()
+
+    def next_12h(self):
+        pass
+
+    def next_12h_simplified(self):
+        pass
